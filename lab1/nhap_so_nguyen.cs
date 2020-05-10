22747b260a218048149f62ded152026937654108
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-10 20:43:07 +0700

    Create nhap_so_nguyen.cs

---
+++ lab1/nhap_so_nguyen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part4
+{
+    class nhap_so_nguyen
+    {
+        static void Main1(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("nhap vao 3 so.");
+            Console.Write("a = ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            b = Int32.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            c = Int32.Parse(Console.ReadLine());
+            if(a > b && a > c)
+            {
+                Console.WriteLine("{0} la so lon nhat", a);
+            }else if(b > c && b > a)
+            {
+                Console.WriteLine("{0} la so lon nhat", b);
+            }else if (c > a && c > b)
+            {
+                Console.WriteLine("{0} la so lon nhat", c);
+            }
+
+        }
+    }
+}
