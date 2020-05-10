45c53ab5e5cb6c92457f59f90e859d03e6d92ac3
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-10 20:42:30 +0700

    Create bai2.cs

---
+++ lab1/bai2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1_1
+{
+    class bai2
+    {
+        static void Main1(string[] args)
+        {
+            int intVal;
+            double dblVal;
+            string strVal;
+            intVal = 10;
+            dblVal = 3.142;
+            strVal = "Fpt Aptech";
+            Console.WriteLine("{0} is an integer value", intVal);
+            Console.WriteLine("{0} is an double value", dblVal);
+            Console.WriteLine("{0} is an string", strVal);
+            Console.ReadLine();
+        }
+    }
+}
