d2e9e0c824c5e452e6ef7ee26e90c0e1b0d97399
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-10 20:42:26 +0700

    Create bai1.cs

---
+++ lab1/bai1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1_1
+{
+    class bai1
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Nguyễn Huy Toản");
+            Console.ReadLine();
+        }
+    }
+}
