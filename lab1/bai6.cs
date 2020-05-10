7ad01436b1acfff1d035b3fdf676319fb9620d22
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-10 20:42:53 +0700

    Create bai6.cs

---
+++ lab1/bai6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1_1
+{
+    class bai6
+    {
+        static void Main1(string[] args)
+        {
+            var i = 5;
+            var s = "hello";
+            var d = 1.0;
+            //i is an integer
+            Console.WriteLine("i*i: " + i * i);
+            //s is a string
+            Console.WriteLine("s in upper case:" + s.ToUpper());
+            //d is a double
+            Console.WriteLine("type of d:" + d.GetType());
+            Console.ReadLine();
+        }
+    }
+}
