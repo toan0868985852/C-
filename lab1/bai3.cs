a22356c49404818c321315fad2cccf40f13f755d
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-10 20:42:42 +0700

    Create bai3.cs

---
+++ lab1/bai3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1_1
+{
+    class bai3
+    {
+        static void Main1(string[] args)
+        {
+
+            int valueVal = 5;
+            Console.WriteLine("The value of the variable is {0}", valueVal);
+            Test(valueVal);
+            Console.ReadLine();
+
+        }
+        static void Test(int valueVal)
+        {
+            int team = 5;
+            valueVal = team * 2;
+            Console.WriteLine(valueVal);
+        }
+    }
+}
