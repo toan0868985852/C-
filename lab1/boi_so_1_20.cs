b26916fb84dcedba0a17ac492ab02ad70bcdc235
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-10 20:43:02 +0700

    Create boi_so_1_20.cs

---
+++ lab1/boi_so_1_20.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boi_so_1_20
+{
+    class boi_so_1_20
+    {
+        static void Main(string[] args)
+        {
+            long number = 1;
+            for(int i = 1; i <= 20; i++)
+            {
+                number = number * i;
+                Console.WriteLine(i + " = " + number);
+            }
+        }
+    }
+}
