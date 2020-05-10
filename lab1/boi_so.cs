88e60952e90733eee30a31345370c65bcd30c688
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-10 20:42:59 +0700

    Create boi_so.cs

---
+++ lab1/boi_so.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part4
+{
+    class boi_so
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap vao mot so: ");
+            int number = Int32.Parse(Console.ReadLine());
+            for(int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine(number + " * " + i + " = " + number * i);
+            }
+        }
+    }
+}
