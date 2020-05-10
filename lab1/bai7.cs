3c52b763126954cc25d95c845388e12ca1d08639
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-10 20:42:57 +0700

    Create bai7.cs

---
+++ lab1/bai7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1_1
+{
+    class bai7
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new { Name = "A", Price = 3 };
+            Console.WriteLine("Name = {0}\nPrice = {1}",p1.Name.ToLower(), p1.Price);
+            Console.ReadLine();
+        }
+    }
+}
