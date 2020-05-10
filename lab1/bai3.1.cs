1de6891a9d1e9015509d358b972143bc0b8d4e32
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-10 20:42:36 +0700

    Create bai3.1.cs

---
+++ lab1/bai3.1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1_1
+{
+    class ReferenceType
+    {
+        public int valueVal;
+    }
+    class Class2
+    {
+        static void Main1(string[] args)
+        {
+            ReferenceType refer = new ReferenceType();
+            refer.valueVal = 5;
+            Test(refer);
+            Console.WriteLine("The value of the variable is {0}", refer.valueVal);
+            Console.ReadLine();
+        }
+        static void Test(ReferenceType refer)
+        {
+            int temp = 5;
+            refer.valueVal = temp * 2;
+        }
+    }
+}
