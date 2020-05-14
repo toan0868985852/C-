888fc2b3d7026793f5544fde1f30cc9bb0dfba9b
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-14 18:51:21 +0700

    Create Button.cs

---
+++ lab3/Button.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bai2
+{
+    class Button : window
+    {
+        public Button(int top, int left) : base(top, left)
+        {
+        }
+        // an overridden version (note keyword) because in the
+        // derived method we change the behavior
+        public override void DrawWindow()
+        {
+            Console.WriteLine("Drawing a button at {0}, {1}\n", top, left);
+        }
+    }
+}
