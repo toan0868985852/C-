0ac6fd3f0c76383a55a99840c4b6d9059403b9fd
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-14 18:51:28 +0700

    Create emloyee.cs

---
+++ lab3/emloyee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace bai4
+{
+    class emloyee
+    {
+
+        private string fistname;
+        private string lastname;
+        private string address;
+        private string sin;
+        private double salary;
+
+        public emloyee(string fistname, string lastname, string address, string sin, double salary)
+        {
+            this.fistname = fistname;
+            this.lastname = lastname;
+            this.address = address;
+            this.sin = sin;
+            this.salary = salary;
+        }
+
+        public virtual void Salary(double discount)
+        {
+            Console.Write("You slary is: ");
+            Console.WriteLine(salary * discount / 100);
+        }
+
+        public override string ToString()
+        {
+            
+            return base.ToString();
+        }
+        
+    }
+}
