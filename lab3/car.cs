9473af99bce174300dd7e2605462c4069ffc526e
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-14 18:51:24 +0700

    Create car.cs

---
+++ lab3/car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace bai1
+{
+    class car
+    {
+        public string make;
+        public string model;
+        public string color;
+        public int yearBuilt;
+        // define the methods
+        public void Start()
+        {
+            System.Console.WriteLine(model + " started");
+        }
+        public void Stop()
+        {
+            System.Console.WriteLine(model + " stopped");
+        }
+        
+    }
+
+
+    }
