ed6e2d4da08fc7c1107033990faad532ba7c48bb
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-14 18:51:42 +0700

    Create window.cs

---
+++ lab3/window.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace bai2
+{
+    class window
+    {
+        public window (int top, int left)
+        {
+            this.top = top;
+            this.left = left;
+        }
+        // simulates drawing the window
+        public virtual void DrawWindow()
+        {
+            Console.WriteLine("Window: drawing Window at {0}, {1}",top, left);
+        }
+        // these members are protected and thus visible
+        // to derived class methods. We'll examine this
+        // later in the chapter
+        protected int top;
+        protected int left;
+    }
+}
