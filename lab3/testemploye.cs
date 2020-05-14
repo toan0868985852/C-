07a8bf9a52e85fcfc20a58e10e656d97cf7c3f1b
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-14 18:51:39 +0700

    Create testemploye.cs

---
+++ lab3/testemploye.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bai4
+{
+    class testemploye
+    {
+        static object Main(string[] args)
+        {
+            string fistname;
+            string lastname;
+            string address;
+            string sin;
+            double salary;
+
+            
+            Console.Write("Enter fist name: ");
+            fistname = Console.ReadLine();
+            Console.Write("Enter lastname: ");
+            lastname = Console.ReadLine();
+            Console.Write("Enter address: ");
+            address = Console.ReadLine();
+            Console.Write("Enter sin: ");
+            sin = Console.ReadLine();
+            Console.Write("Enter salary: ");
+            salary = double.Parse(Console.ReadLine());
+
+            emloyee e = new emloyee(fistname, lastname, address, sin, salary);
+            double discount;
+            Console.Write("Enter discount: ");
+            discount = double.Parse(Console.ReadLine());
+
+           
+            e.Salary(discount);
+        }
+    }
+}
