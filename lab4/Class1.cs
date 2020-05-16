ef7c50c1e9f3c951d0efb23b13d2501213e9ad47
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-16 17:49:33 +0700

    Create Class1.cs

---
+++ lab4/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace people
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee("E1", 400000, "Tran van a", "thanh hoa", "tran@gmail.com");
+            Console.WriteLine("Employee: " + e1.ToString());
+            e1.getsetEmail = "tran123@gmail.com";
+            e1.getsetAddress = "Ha Noi";
+            Console.WriteLine("Employee after: " + e1.ToString());
+            Console.WriteLine("Name: " + e1.getName);
+            Console.WriteLine("Address: " + e1.getsetAddress);
+            Console.WriteLine("Email: " + e1.getsetEmail);
+            Console.WriteLine("department: " + e1.getDepartment);
+            Console.WriteLine("salary: " + e1.getSalary);
+            Console.ReadKey();
+        }
+    }
+}
