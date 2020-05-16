a63694d9bb6f6fa8a2a83354abe2b4f36c2dc100
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-16 17:49:46 +0700

    Create Person.cs

---
+++ lab4/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace people
+{
+     public abstract class Person
+    {
+        protected String name;
+        protected string address;
+        protected string email;
+
+        protected Person() { 
+        
+        }
+
+        protected Person(string name, string address, string email)
+        {
+            this.name = name;
+            this.address = address;
+            this.email = email;
+        }
+
+        public string getName 
+        {
+            get { return name; }
+        }
+
+        public string getsetAddress
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string getsetEmail
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public abstract double Faculty();
+        public abstract double Staff();
+
+
+
+    }
+
+    
+}
