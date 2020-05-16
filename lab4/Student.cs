9457545b985a6bffa2cfce7de9c977b77f53a68e
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-16 17:49:53 +0700

    Create Student.cs

---
+++ lab4/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace people
+{
+    public class Student : Person
+    {
+        private string Business;
+        private string Computer_Science;
+
+        public Student(string Business, string Computer_Science)
+        {
+            this.Business = Business;
+            this.Computer_Science = Computer_Science;
+        }
+
+        public Student(string Business, string Computer_Science, string name, string address, string email) : base(name, address, email)
+        {
+            this.Business = Business;
+            this.Computer_Science = Computer_Science;
+        }
+
+        public override string ToString()
+        {
+            return "Student has: Business is" + Business + ", Computer Science" + Computer_Science + ", name is " + name + ", address is " + address + ", email is " + email;
+        }
+
+
+    }
+
+
+}
