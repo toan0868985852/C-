b25fc657114eee67eecb687a4a93bfd7cac2601d
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-16 17:49:36 +0700

    Create Employee.cs

---
+++ lab4/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace people
+{
+    public class Employee : Person
+    {
+        private string department;
+        private double salary;
+    
+        public Employee(string department, double salary)
+        {
+            this.department = department;
+            this.salary = salary;
+        }
+
+        public Employee(string department, double salary, string name, string address, string email) : 
+            base(name, address, email)
+        {
+            this.department = department;
+            this.salary = salary;
+        }
+
+        public string getDepartment
+        {
+            get { return department; }
+ 
+        }
+
+        public double getSalary
+        {
+            get { return salary; }
+       
+        }
+
+        public override string ToString()
+        {
+            return "Student has: Business is" + department + ", Computer Science" + salary + ", name is " + name + ", address is " + address + ", email is " + email;
+        }
+
+        public override double Faculty()
+        {
+            return 1000 + 0.05 * salary;
+        }
+
+        public override double Staff()
+        {
+            return 0.06 * salary;
+        }
+
+    }
+}
