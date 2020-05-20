3d3353e13d473409e73e3a2c2fdb01bf6a9e3b95
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-20 14:09:23 +0700

    Create BusinessEmployee.cs

---
+++ lab5/BusinessEmployee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract
+{
+    class BusinessEmployee : Employee
+    {
+        public double bonusBudget = 1000;
+
+        // Calls upon base-class (Employee Class) constructor from within derived class (BusinessEmployee Class)
+        // Sets baseSalary to 50000 for all BusinessEmployee objects
+        public BusinessEmployee(String name) : base(name, 50000)
+        {
+        }
+        public override String employeeStatus()
+        {
+            return toString() + " with a budget of " + this.bonusBudget;
+        }
+    }
+}
