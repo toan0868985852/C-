39dd615cef315f2240067a48959e49280ad6a417
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-20 14:09:31 +0700

    Create program.cs

---
+++ lab5/program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates TechnicalEmployee Object with name Libby called employee1
+            var employee1 = new TechnicalEmployee("Libby");
+            // Instantiates TechnicalEmployee Object with name Zaynah called employee2
+            var employee2 = new TechnicalEmployee("Zaynah");
+            // Instantiates BusinessEmployee Object with name Winter called employee3
+            var employee3 = new BusinessEmployee("Winter");
+
+            Console.WriteLine(employee1.employeeStatus() + "..." + employee2.employeeStatus() + "..." + employee3.employeeStatus());
+        }
+    }
+}
