3c89098e6d04accd6a1172ea40d8e1a1de606ff8
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-20 14:09:27 +0700

    Create Employee.cs

---
+++ lab5/Employee.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Abstract
+{
+   abstract class Employee
+    {
+
+        private static int employeeCount = 1;
+        private string employeeName;
+        private double employeeBaseSalary;
+        private int employeeId;
+
+        // Public properties
+        public string Name
+        {
+            get
+            {
+                return employeeName;
+            }
+
+            set
+            {
+                employeeName = value;
+            }
+        }
+        public double BaseSalary
+        {
+            get
+            {
+                return employeeBaseSalary;
+            }
+
+            set
+            {
+                employeeBaseSalary = value;
+            }
+        }
+        public int ID
+        {
+            get
+            {
+                return employeeId;
+            }
+
+            set
+            {
+                employeeId = value;
+            }
+        }
+
+        // Constructor
+        public Employee(String name, double baseSalary)
+        {
+            this.Name = name;
+            this.BaseSalary = baseSalary;
+            this.ID = employeeCount++;
+        }
+
+        // This method returns the employee's base salary
+        public double getBaseSalary()
+        {
+            return this.BaseSalary;
+        }
+
+        // This method returns the employee's name
+        public String getName()
+        {
+            return this.Name;
+        }
+
+        // This method returns the employee's ID
+        public int getEmployeeID()
+        {
+            return this.ID;
+        }
+
+        // This method returns the employee's ID and Name
+        public String toString()
+        {
+            return this.ID + " " + this.Name;
+        }
+
+        // Abstract method
+        public abstract String employeeStatus();
+    }
+}
+
