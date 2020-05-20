5f6b0c7c61a5d33b5bd91d51b3abeef91625d889
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-20 14:09:19 +0700

    Create TechnicalEmployee.cs

---
+++ lab5/TechnicalEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract
+{
+    class TechnicalEmployee : Employee
+    {
+        public int successfulCheckIns = 5;
+
+        // Calls upon base-class (Employee Class) constructor from within derived class (TechnicalEmployee Class)
+        // Sets baseSalary to 75000 for all TechnicalEmployee objects
+        public TechnicalEmployee(String name) : base(name, 75000)
+        {
+           
+        }
+
+        public override String employeeStatus()
+        {
+            return this.toString() + " has " + this.successfulCheckIns + " successful check ins";
+        }
+
+    }
+}
