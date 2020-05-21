a790f480955746f47acbb5107635bf7749e9eb5d
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-05-21 17:30:23 +0700

    lab6

---
+++ lab6/Customer_name.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Customer;
+using Order;
+
+namespace Customer
+{
+    class Customer_name
+    {
+        public string nameCustomer;
+
+        public void getName()
+        {
+            Console.WriteLine("Nhap ten : ");
+            nameCustomer = Console.ReadLine();
+        }
+    }
+}
+
+namespace Order
+{
+    class hang_tap_hoa
+    {
+        public void Order_hang_tap_hoa()
+        {
+            Customer_name c1 = new Customer_name();
+            c1.getName();
+            Console.Write("{0} ", c1.nameCustomer);
+            Console.WriteLine("ban da mua san pham tap hoa");
+        }
+    }
+    class hang_banh_keo
+    {
+        public void Order_hang_banh_keo()
+        {
+            Customer_name c2 = new Customer_name();
+            c2.getName();
+            Console.Write("{0} ", c2.nameCustomer);
+            Console.WriteLine("ban da mua san pham banh_keo");
+        }
+    }
+}
+
---
+++ lab6/testOrder.cs
@@ -0,0 +1,38 @@
+﻿using Order;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oder
+{
+    class testOrder
+    {
+        public static void Main()
+        {
+            string lua_chon;
+
+            Console.WriteLine("chon san pham: ");
+            Console.WriteLine("1-hang tap hoa ");
+            Console.WriteLine("2-hang banh keo ");
+
+            lua_chon = Console.ReadLine();
+            if(lua_chon == "1")
+            {
+                hang_tap_hoa h1 = new hang_tap_hoa();
+                h1.Order_hang_tap_hoa();
+            }
+            else
+            {
+                if(lua_chon == "2")
+                {
+                    hang_banh_keo h2 = new hang_banh_keo();
+                    h2.Order_hang_banh_keo();
+                }
+                else
+                {
+                    Console.WriteLine("Chon mat hang: 1 hoac 2");
+                }
+            }
+        }
+    }
+}
